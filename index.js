2f0575bae28eb6334b7930d510ffdc4b4931bd49
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-18 13:53:02 -0400

    commit tree d79f363fcdeecbe73ad57a4d925ac0fc2d061195

---
+++ index.js
@@ -0,0 +1,13 @@
+// const divide = function() {
+//   return 2000 / 100;
+// }
+
+// const square = parameter => parameter * parameter;
+
+// const add = (parametr1, paramentr2) => parametr1 + paramentr2;
+const divide = function () {
+  return 2000 / 100;
+};
+
+const square = (parameter) => parameter * parameter;
+const add = (parameter1, parameter2) => parameter1 + parameter2;
